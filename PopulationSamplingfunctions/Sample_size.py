8728c63f79a914f5f22bafdd099b1a148f4f8877
Juan Benavides <jb493@njit.edu>
2020-03-12 22:34:55 -0400

    Reorganized Sample Sizes to be in same class

---
+++ PopulationSamplingfunctions/Sample_size.py
@@ -0,0 +1,31 @@
+from Calculator.SquareRoot import squareRoot
+from Calculator.Division import division
+from Calculator.Subtraction import subtraction
+from Statistics.Z_score import z_scores
+from PopulationSamplingfunctions.Margin_error import MarginError
+
+
+class SampleSize:
+    @staticmethod
+    def unknown_pop_sample(data, seed, percent):
+        z_s = z_scores(data, seed)
+        m_e = MarginError.margin(data, seed)
+        p = percent
+        q = subtraction(1, p)
+
+        val = division(z_s, m_e)
+        samplePop = squareRoot(val) * p * q
+
+        return samplePop
+
+    @staticmethod
+    def known_pop_sample(data, seed):
+        z_s = z_scores(data, seed)
+        m_e = MarginError.margin(data, seed)
+        s_d = standard_deviation(data)
+
+        value = (z_s * s_d) / m_e
+
+        popSample = squareRoot(value)
+
+        return popSample
