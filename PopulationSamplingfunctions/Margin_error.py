0a5281b7076ce2b5a286a5a2cd53b0ea6ddaf477
jdl49 <55502428+jdl49@users.noreply.github.com>
2020-03-12 19:46:14 -0500

    Update Margin_error.py

--- PopulationSamplingfunctions/Margin_error.py
+++ PopulationSamplingfunctions/Margin_error.py
@@ -1,1 +1,12 @@
-s
+from Statistics.Standard_Deviation import standard_deviation
+from Statistics.Z_score import z_scores
+from Calculator.Multiplication import multiplication
+
+
+class MarginError:
+    @staticmethod
+    def margin(data, seed):
+        zs = z_scores(data, seed)
+        sd = standard_deviation(data)
+        margin = multiplication(zs, sd)
+        return margin
