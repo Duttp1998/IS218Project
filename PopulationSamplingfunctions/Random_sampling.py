6e604e701bf1530f79f0e4f790df453866503c2c
jdl49 <jdl49@njit.edu>
2020-03-13 20:39:24 -0500

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	Statistics/PopulationCorrelation.py

--- PopulationSamplingfunctions/Random_sampling.py
+++ PopulationSamplingfunctions/Random_sampling.py
@@ -1,10 +1,11 @@
 from Random.Numbers import Numbers
+
+
 class RandomSample:
-   @staticmethod
+    @staticmethod
     def simpleSample(array, n):
         result = []
         while n >= 0:
             result.append(array.pop(Numbers.randINT(0, len(array) - 1)))
             n -= 1
         return result
-
