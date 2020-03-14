7dcc283351d8984c521608b49205f52a582ba7e8
Dutt Patel <duttp1998@gmail.com>
2020-03-13 22:12:54 -0400

    .

--- Tests/test_statistics.py
+++ Tests/test_statistics.py
@@ -27,27 +27,27 @@ class MyTestCase(unittest.TestCase):
 
     def test_mean_calculator(self):
         mean = Mean.mean(self.testData)
-        self.assertEqual(mean, 14)
+        self.assertEqual(mean, 54.47)
 
     def test_median_calculator(self):
         median = Median.median(self.testData)
-        self.assertEqual(median, 14.5)
+        self.assertEqual(median, 57.0 )
 
     def test_mode_calculator(self):
         mode = Mode.mode(self.testData)
-        self.assertEqual(mode, 10)
+        self.assertEqual(mode, 98)
 
     def test_variance_calculator(self):
         variance = Variance.variance(self.testData)
-        self.assertEqual(variance, 11)
+        self.assertEqual(variance, 884.1708080808081)
 
     def test_skewness_calculator(self):
         skew = Skewness.skewness(self.testData)
-        self.assertEqual(skew, 0.03576490724804414)
+        self.assertEqual(skew, -0.21197867987065927)
 
     def test_standarddeviation_calculator(self):
         standard = Standarddeviation.standarddeviation(self.testData)
-        self.assertEqual(standard, 3.3166247903554)
+        self.assertEqual(standard, 29.735009804619338)
 
     def test_quartiles_calculator(self):
         quart = Quartiles.quartiles(self.testData)
@@ -55,11 +55,11 @@ class MyTestCase(unittest.TestCase):
 
     def test_zscore_calculator(self):
         zscore = Zscore.zscore(self.testData)
-        self.assertEqual(zscore, -1.276398025379199)
+        self.assertEqual(zscore, -1.2326792344561226)
 
     def test_meandeviation_calculator(self):
         meand = Meandeviation.meandeviation(self.testData)
-        self.assertEqual(meand, 3.033333333333333)
+        self.assertEqual(meand, 25.09240000000001)
 
     def test_samplecorrelation_calculator(self):
         samplec = Samplecorrelation.samplecorrelation(self.testData)
